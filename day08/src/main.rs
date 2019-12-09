94872b2b40299435b6b0a415d7d2c6a030475c6f
Eric Stolten <stoltene2@gmail.com>
2019-12-08 20:39:33 -0500

    Finish day08
    I thought this was a dumb exercise

--- day08/src/main.rs
+++ day08/src/main.rs
@@ -1,7 +1,19 @@
 use itertools::Itertools;
 
 fn main() {
-    println!("{:?}", verify_image(&image_input(), 25, 6));
+    let r = input_to_layer_vec(&image_input(), 25, 6);
+
+    // Print the "secret" message. Use ' ' for white and * for black
+    for j in 0..6 {
+        for i in 0..25 {
+            if r[i+(j*25)] == 0 {
+                print!("{}", " ");
+            } else {
+                print!("{}", "*");
+            }
+        }
+        println!("");
+    }
 }
 
 fn verify_image(image: &Vec<u8>, x: usize, y: usize) -> u32 {
@@ -49,7 +61,45 @@ fn count_0_1_2(row: Vec<&u8>) -> (u32, u32, u32) {
     })
 }
 
+// Takes input and makes a Vec of Vecs where each row is the flattened layer
+fn input_to_layer_vec(input: &Vec<u8>, row_size: usize, column_size: usize) -> Vec<u8> {
+    let image_size = row_size * column_size;
+    let mut layers: Vec<_> = Vec::new();
+
+    for layer in &input.into_iter().chunks(image_size) {
+        let vec = layer.collect_vec();
+        layers.push(vec);
+    }
+
+    let mut output = Vec::new();
+
+    for col in 0..image_size {
+        let mut nth_element_from_each_layer = Vec::new();
+
+        for layer_num in 0..layers.len() {
+            nth_element_from_each_layer.push(*layers[layer_num][col]);
+        }
+
+        output.push(first_non_transparent(nth_element_from_each_layer).unwrap_or(2));
+    }
+
+    println!("output size: {}", &output.len());
+    output
+}
+
+fn first_non_transparent(nums: Vec<u8>) -> Option<u8> {
+    let ns: Vec<u8> = nums.into_iter().skip_while(|&x| x == 2).collect();
+
+    match ns.get(0) {
+        Some(val) => Some(*val),
+        None => None,
+    }
+}
+
+// Returns image_input so that it is 0, 1, 2. Instead of byte order of 48, 49, 50
 fn image_input() -> Vec<u8> {
     use std::fs;
-    fs::read("input.txt").unwrap()
+    let mut input = fs::read("input.txt").unwrap();
+    input.pop();
+    input.into_iter().map(|n| n-48).collect()
 }
