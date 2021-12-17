091f7e07f8e02a6cbed6ad98ecc468c0bdd8e68c
Eric Stolten <stoltene2@gmail.com>
2021-12-16 22:16:42 -0500

    Day 16 part 1

--- 2021/src/bin/day-16.rs
+++ 2021/src/bin/day-16.rs
@@ -19,6 +19,25 @@ enum Packet {
 const LITERAL: usize = 4;
 
 impl Packet {
+    fn sum_versions(&self) -> usize {
+        match self {
+            Packet::Literal { version, value: _ } => *version,
+            Self::OperatorFixedSize {
+                version,
+                sub_packets,
+            } => {
+                let sub_packet_total: usize = sub_packets.iter().map(|p| p.sum_versions()).sum();
+                *version + sub_packet_total
+            }
+            Self::OperatorFixedCount {
+                version,
+                sub_packets,
+            } => {
+                let sub_packet_total: usize = sub_packets.iter().map(|p| p.sum_versions()).sum();
+                *version + sub_packet_total
+            }
+        }
+    }
     fn parse(mut s: Bits) -> (Packet, Bits) {
         // print!("Parsing: ");
         // pp_bits(&s);
@@ -186,6 +205,35 @@ fn main() {
         },
         r.0
     );
+
+    let binary_str = hex_string_to_binary_string(&String::from(input()));
+    let r = Packet::parse(binary_str);
+
+    //println!("\ninput: {:?}", &r);
+    let total = r.0.sum_versions();
+    println!("sum: {}", total);
+    assert_eq!(940, total);
+
+    // assert_eq!(
+    //     Packet::OperatorFixedCount {
+    //         version: 7,
+    //         sub_packets: vec![
+    //             Packet::Literal {
+    //                 version: 2,
+    //                 value: 1
+    //             },
+    //             Packet::Literal {
+    //                 version: 4,
+    //                 value: 2
+    //             },
+    //             Packet::Literal {
+    //                 version: 1,
+    //                 value: 3
+    //             }
+    //         ]
+    //     },
+    //     r.0
+    // );
 }
 
 fn hex_string_to_binary_string(s: &String) -> Bits {
@@ -231,4 +279,8 @@ fn hex_char_to_binary_string(c: char) -> Bits {
     };
 
     s.chars().collect()
+}
+
+fn input() -> &'static str {
+    "E0525D9802FA00B80021B13E2D4260004321DC648D729DD67B2412009966D76C0159ED274F6921402E9FD4AC1B0F652CD339D7B82240083C9A54E819802B369DC0082CF90CF9280081727DAF41E6A5C1B9B8E41A4F31A4EF67E2009834015986F9ABE41E7D6080213931CB004270DE5DD4C010E00D50401B8A708E3F80021F0BE0A43D9E460007E62ACEE7F9FB4491BC2260090A573A876B1BC4D679BA7A642401434937C911CD984910490CCFC27CC7EE686009CFC57EC0149CEFE4D135A0C200C0F401298BCF265377F79C279F540279ACCE5A820CB044B62299291C0198025401AA00021D1822BC5C100763A4698FB350E6184C00A9820200FAF00244998F67D59998F67D5A93ECB0D6E0164D709A47F5AEB6612D1B1AC788846008780252555097F51F263A1CA00C4D0946B92669EE47315060081206C96208B0B2610E7B389737F3E2006D66C1A1D4ABEC3E1003A3B0805D337C2F4FA5CD83CE7DA67A304E9BEEF32DCEF08A400020B1967FC2660084BC77BAC3F847B004E6CA26CA140095003900BAA3002140087003D40080022E8C00870039400E1002D400F10038C00D100218038F400B6100229500226699FEB9F9B098021A00800021507627C321006E24C5784B160C014A0054A64E64BB5459DE821803324093AEB3254600B4BF75C50D0046562F72B1793004667B6E78EFC0139FD534733409232D7742E402850803F1FA3143D00042226C4A8B800084C528FD1527E98D5EB45C6003FE7F7FCBA000A1E600FC5A8311F08010983F0BA0890021F1B61CC4620140EC010100762DC4C8720008641E89F0866259AF460C015D00564F71ED2935993A539C0F9AA6B0786008D80233514594F43CDD31F585005A25C3430047401194EA649E87E0CA801D320D2971C95CAA380393AF131F94F9E0499A775460"
 }
